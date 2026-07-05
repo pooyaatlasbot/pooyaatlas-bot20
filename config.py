82007fd00b32b1c823ef308e131a43ddffd1c9a7
pooyaatlasbot <mrsheikhali2021@gmail.com>
2026-07-06 00:32:02 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+SCHOOL_NAME = "آموزشگاه خلبانی پویا اطلس"
+
+PHONE = "02634490401"
+
+WEBSITE = "https://pooyaflight.ir"
+
+ADDRESS = """کرج
+مطهری شمالی
+بین خیابان پیروزی و آزادی
+ساختمان وکلا
+پلاک ۱۶۶
+واحد ۳"""
